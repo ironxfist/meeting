927c1e5cc135397de1cb80b1ece57311a44c9bd9
Cherie Cheung <cheriecheung@abtitu.com>
2020-04-14 12:24:57 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { meetings } from "./meetings";
 
 function App() {
+  const [sorting, setSorting] = useState("none");
+
+  const sortMeetingsByDay = (array) => {
+    const sorter = {
+      Sunday: 0,
+      Monday: 1,
+      Tuesday: 2,
+      Wednesday: 3,
+      Thursday: 4,
+      Friday: 5,
+      Saturday: 6,
+    };
+
+    return array
+      .map((item) => [item.day, item])
+      .sort(([a], [b]) => sorter[a] - sorter[b])
+      .map(([, item]) => item);
+  };
+
+  const sortedDays = sortMeetingsByDay(meetings);
+
+  const sortMeetingsByTime = (array) => {
+    return array
+      .map((item) => [Date.parse(`2020/04/04 ${item.time}`), item])
+      .sort(([a], [b]) => a - b)
+      .map(([, item]) => item);
+  };
+
+  const sortedTime = sortMeetingsByTime(meetings);
+
+  const tableContent = () => {
+    switch (sorting) {
+      case "none":
+        return meetings;
+      case "day":
+        return sortedDays;
+      case "time":
+        return sortedTime;
+      default:
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        fontSize: "0.7rem",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
+      <table cellPadding="5" width="40%">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>
+              <button
+                onClick={() => setSorting(sorting === "day" ? "none" : "day")}
+              >
+                Day
+              </button>
+            </th>
+            <th>
+              <button
+                onClick={() => setSorting(sorting === "time" ? "none" : "time")}
+              >
+                Time
+              </button>
+            </th>
+            <th>Platform</th>
+            <th>Dial Number</th>
+            <th>Date</th>
+          </tr>
+          {tableContent().map((meeting) => {
+            return (
+              <tr key={meeting.name}>
+                <td>{meeting.name}</td>
+                <td>{meeting.day}</td>
+                <td>{meeting.time}</td>
+                <td>{meeting.platform}</td>
+                <td>{meeting.dial_number}</td>
+                <td>{meeting.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
